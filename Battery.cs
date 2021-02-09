bfee1273c163a39b19fe0c249fb6f89850c53237
Eric <eric2.0turcotte@gmail.com>
2021-02-08 21:12:53 -0500

    first push

---
+++ Battery.cs
@@ -0,0 +1,16 @@
+
+
+public class Battery
+{
+    public int id;
+    public int amountOfColumns;
+    public string status;
+    public int amountOfFloors;
+    public int amountOfBasements;
+    public int amountOfElevatorPerColumn;
+
+    // columnList
+    // callButtonsList
+
+    
+}
